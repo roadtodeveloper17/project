01b20c70d9d8b281c694574da36211886f17a6ce
Sayat Turgynbay <sayatturgynbay123@gmail.com>
2026-06-05 14:26:03 +0500

    Add database models and initial migration

--- app/database.py
+++ app/database.py
@@ -1,10 +1,15 @@
+import os
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = ""
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(autocommit= False, autoflush= False ,bind=engine)
 Base = declarative_base()
 
 def get_db():
---
+++ app/models.py
@@ -0,0 +1,50 @@
+from app.database import Base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key = True, index = True)
+
+    email= Column(String, unique = True, nullable= False, index= True)
+    hashed_password = Column(String, nullable= False)
+    role = Column(String, default= "user", nullable= False)
+    created_at = Column(DateTime(timezone= True), server_default=func.now())
+
+    projects = relationship("Project", back_populates="owner")
+    assigned_tasks = relationship("Task", back_populates = "assigned_user")
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id = Column(Integer, primary_key = True, index= True)
+
+    name = Column(String, nullable= False)
+    description = Column(String, nullable= True)
+
+    owner_id = Column(Integer, ForeignKey("users.id"), nullable= False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    owner = relationship("User", back_populates = "projects")
+    tasks = relationship("Task", back_populates= "project")
+
+class Tasks(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key = True, index = True)
+
+    title = Column(String, nullable= False)
+    description = Column(String, nullable= True)
+    status = Column(String, default= "todo", nullable= False)
+
+    project_id = Column(Integer, ForeignKey("projects.id"), nullable= False)
+    assigned_user_id = Column(Integer, ForeignKey("users.id"), nullable= False)
+    
+    created_at = Column(DateTime(timezone= True), server_default=func.now())
+
+    assigned_user = relationship("User", back_populates = "assigned_tasks")
+    project = relationship("Project", back_populates= "tasks")
+    
+
